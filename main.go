c2eed92a3cbe2bdd4f70cb44de01226f25e7ca42
Mostafa Ali Mansour <tifa64@users.noreply.github.com>
2021-10-04 20:50:19 +0200

    Adding Name server utility

---
+++ main.go
@@ -0,0 +1,48 @@
+
+   
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"log"
+	"net"
+	"os"
+)
+
+func main(){
+	app := cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Let's you query IPs, CNAMs, MX records and Name Servers"
+
+	myFlags := []cli.Flag{
+		&cli.StringFlag{
+			Name: "host",
+			Value: "google.com",
+		},
+	}
+
+	app.Commands = []*cli.Command{
+		{
+			Name: "ns",
+			Usage: "Looks up the Name Servers for a Particular Host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+					return err
+				}
+				for i:=0; i<len(ns); i++{
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		}
+	}
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+}
